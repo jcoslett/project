27d8d0abf0da680be212e7f039896558c6febcc5
James Coslett <james.coslett@gmail.com>
2016-01-30 22:07:26 -0800

    Making gamestate object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,55 @@
-console.log("JS Loaded");
-//Create initial variables:
+
+
+  /* MODEL: Data Model **************************************************/
+  // make a single global-level object on window for the game…
+  window.game = {
+    timer: undefined,
+    speed: 900,
+    isWon: false,
+    isPaused: true,
+    board: {
+      openCells:   [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      closedCells: [],
+      avocadoCell: undefined
+    },
+    printState: function() {
+      // console.log(this)
+      if (game.won) {
+        console.log("YOU WON!");
+      } else {
+        console.log(
+          "-> Avocado: " + (game.avocadoCell === undefined ? "-" : game.avocadoCell),
+          " (P: " + game.isPaused + ")",
+          "\n   Open:    " + "[" + game.openCells + "]",
+          "\n   Closed:  " + "[" + game.closedCells + "]"
+        );
+      }
+    }
+  };
+
+window.gameState = {
+  timer: undefined,
+  speed: 2000,
+  isInProgress: false,
+  canGuess: false,
+  display: {
+    litCell: undefined,
+    currentSound: undefined
+  },
+  printState: function() {
+    // console.log(this)
+    if (!gameState.isInProgress) {
+      console.log("Not currently training");
+    } else {
+      console.log(
+        "Lit cell: " + (gameState.litCell === undefined ? "-" : gameState.litCell),
+        " (P: " + game.isPaused + ")",
+        "\n   Open:    " + "[" + game.openCells + "]",
+        "\n   Closed:  " + "[" + game.closedCells + "]"
+      );
+    }
+  }
+}
 var audioGens = [];
 var videoGens = [];
 var audioResps = [];
