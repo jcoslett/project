fe18eef218a608a82713a913c6705de52d07034a
James Coslett <james.coslett@gmail.com>
2016-01-30 18:27:01 -0800

    Worked on random sound generator function

--- js/main.js
+++ js/main.js
@@ -6,7 +6,21 @@ var audioResps = [];
 var videoResps = [];
 var iterationCounter = 0;
 
-function generate() {
+function generateCell(){
   var randomCell = Math.floor(Math.random()*8);
   return randomCell;
 };
+//Generates a random sound name from {tsrkhl}
+function generateSound(){
+  var randomSound = Math.floor(Math.random()*6);
+  if (randomSound = 1) {return "t"};
+  if (randomSound = 2) {return "s"};
+  if (randomSound = 3) {return "r"};
+  if (randomSound = 4) {return "k"};
+  if (randomSound = 5) {return "h"};
+  if (randomSound = 6) {return "l"};
+}
+
+function initialDelay() {
+
+}
