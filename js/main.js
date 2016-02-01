9bde9adc1d893067f47baf1cd05411458760ad27
James Coslett <james.coslett@gmail.com>
2016-02-01 09:52:10 -0800

    More workkkkkk

--- js/main.js
+++ js/main.js
@@ -6,16 +6,18 @@
 window.gameState = {
   flashes: undefined,
   n: 2,
+  trainingPeriod : 20;
   speedSetting: 2000,
   isInProgress: false,
   canGuess: false,
-  display: {
-    litCell: undefined,
-    currentSound: undefined
-  },
-  responses: {
-    currentAudioResp: undefined,
-    currentVideoResp: undefined
+  litCell: undefined,
+  currentSound: undefined
+  currentAudioResp: undefined,
+  currentVideoResp: undefined,
+  audioGens = [];
+  videoGens = [];
+  audioResps = [];
+  videoResps = [];
   }
   printState: function() {
     // console.log(this)
@@ -25,23 +27,26 @@ window.gameState = {
       console.log(
         "Lit cell: " + (gameState.litCell === undefined ? "-" : gameState.litCell),
         "\nCurrent sound: " + (gameState.currentSound === undefined ? "_" : gameState.currentSound),
-        "\nCan guess: " + (game.canGuess === false ? "No" : "Yes" + ),
-        "\nAudio N-Back: " + ( (game.audioGens[flashes-n]) === undefined ? "-" : gameState.audioGens[flashes-n]),
-        "\nVideo N-Back: " + (gameState.videoGens[flashes-n] === undefined ? "-" : gameState.videoGens[flashes-n]),
-        "\nAudio Response: " + ( (game.audioGens[flashes-n]) === undefined ? "-" : gameState.audioGens[flashes-n]),
-        "\nVideo Response: " + (gameState.videoGens[flashes-n] === undefined ? "-" : gameState.videoGens[flashes-n]),
-        "\nAudio Correct: " + ( (game.audioGens[flashes-n]) === undefined ? "-" : gameState.audioGens[flashes-n]),
-        "\nVideo Correct: " + (gameState.videoGens[flashes-n] === undefined ? "-" : gameState.videoGens[flashes-n]),
+        "\nCan guess: " + (game.canGuess === false ? "No" : "Yes"),
+        "\nAudio N-Back: " + ((gameState.canGuess === undefined) || (gameState.videoGens[flashes-n] === undefined)) ? "-" : gameState.audioGens[flashes-n]),
+        "\nVideo N-Back: " + ((gameState.canGuess === undefined) || (gameState.videoGens[flashes-n] === undefined)) ? "-" : gameState.videoGens[flashes-n]),
+        "\nAudio Response: " + (game.currentAudioResp === undefined ? "-" : gameState.currentAudioResp),
+        "\nVideo Response: " + (game.currentVideoResp === undefined ? "-" : gameState.currentVideoResp),
+        "\nAudio Correct: " + ( match(game.currentAudioResp,gameState.audioGens[flashes-n]) === true ? "CORRECT" : "incorrect"),
+        "\nVideo Correct: " + ( match(game.currentAudioResp,gameState.audioGens[flashes-n]) === true ? "CORRECT" : "incorrect"),
       );
     }
   }
 }
-var audioGens = [];
-var videoGens = [];
-var audioResps = [];
-var videoResps = [];
-var iterationCounter = 0;
 
+
+function match(input1,input2){
+  if (input1 === input2){
+    return true;
+  }
+    return false;
+}
+//Generates a random cell index from {0:8}
 function generateCell(){
   var randomCell = Math.floor(Math.random()*8);
   return randomCell;
@@ -53,7 +58,18 @@ function generateSound(){
   return randomSound;
 }
 /*
-function initialDelay() {
+function initialDelay() {*/
+function mainEvent(){
 
 }
+}
 */
+
+Define a turn count, an n-back, random audio, random video, ability to guess, audio response, video response
+Match n back to get current correctness for both
+Display individual affirmation if a match
+Record current correctness for both by pushing onto correctness array
+Do this throughout the training
+total correct responses and percentages
+return to home screen
+
