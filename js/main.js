dd914910c99172c443055a887d4c00aec641b56c
James Coslett <james.coslett@gmail.com>
2016-02-02 14:12:42 -0800

    functionsss

--- js/main.js
+++ js/main.js
@@ -11,30 +11,14 @@ window.gameState = {
   currentSound: undefined,
   currentAudioResp: undefined,
   currentVideoResp: undefined,
+  audioScore: 0,
+  videoScore: 0,
   audioGens: [],
   videoGens: [],
   audioResps: [],
   videoResps: [],
   }
-  /*printState: function() {
-    // console.log(this)
-    if (!gameState.isInProgress) {
-      console.log("Not currently training");
-    } else {
-      console.log(
-        "Lit cell: " + (gameState.litCell === undefined ? "-" : gameState.litCell),
-        "\nCurrent sound: " + (gameState.currentSound === undefined ? "_" : gameState.currentSound),
-        "\nCan guess: " + (game.canGuess === false ? "No" : "Yes"),
-        "\nAudio N-Back: " + ((gameState.canGuess === undefined) || (gameState.videoGens[flashes-n] === undefined)) ? "-" : gameState.audioGens[flashes-n]),
-        "\nVideo N-Back: " + ((gameState.canGuess === undefined) || (gameState.videoGens[flashes-n] === undefined)) ? "-" : gameState.videoGens[flashes-n]),
-        "\nAudio Response: " + (game.currentAudioResp === undefined ? "-" : gameState.currentAudioResp),
-        "\nVideo Response: " + (game.currentVideoResp === undefined ? "-" : gameState.currentVideoResp),
-        "\nAudio Correct: " + ( match(game.currentAudioResp,gameState.audioGens[flashes-n]) === true ? "CORRECT" : "incorrect"),
-        "\nVideo Correct: " + ( match(game.currentAudioResp,gameState.audioGens[flashes-n]) === true ? "CORRECT" : "incorrect"),
-      );
-    }
-  }
-*/
+
 //Generates a random cell index from {0:8}
 function generateCell(){
   var randomCell = Math.floor(Math.random()*8);
@@ -43,15 +27,15 @@ function generateCell(){
 
 //Generates a random sound name from {tsrkhl}
 function generateSound(){
-  var sounds = ["t","s","r","k","h","l"];
+  var sounds = ["m","s","t","f","k","h"];
   var randomSound = sounds[Math.floor(Math.random()*sounds.length)];
   return randomSound;
 }
-
+//delays
 function delay(){
   return true;
 }
-
+//
 function removeDiv(){
 $('td div').remove();
 }
@@ -79,28 +63,51 @@ function mainEvent(){
   for (i=0; i< trainingPeriod+2; i++){
   //move the turn count
   flash++;
-  //generate a sound and make it the current one
-  currentSound = generateSound();
-  //play the sound
-
-  //record the sound generated
-  audioGens[flash-1].push(currentSound);
-  //generate a cell index and make it the current one
-  litCell = generateCell();
-  //light the cell and record it
-
+  //generate a sound, make it the current one, and record it
+  playAndRecord();
+  //generate a cell, make it the current one, and record it
+  lightAndRecord();
   //set canGuess if there have been enough flashes
   if (flash > n){
     canGuess = true;
   }
-  //add a key input listener that records answer
+  //add a key input listener that records answers
   if (canGuess == true){
-
+    $(document).keydown(function(event) {
+      var input = event.which; // return which key was pressed
+      if((input === 81) || (input === 87) || (input === 69) ||
+        (input === 82) || (input === 65) || (input === 83) ||
+        (input === 68) || (input === 70) || (input === 90) ||
+        (input === 88) || (input === 67)){
+          currentAudioResp = true;
+          gameState.videoResps.push(currentVideoResp);
+      } else if ((input === 80) || (input === 79) || (input === 73) ||
+        (input === 85) || (input === 74) || (input === 75) ||
+        (input === 76) || (input === 186) || (input === 77) ||
+        (input === 78) || (input === 188) || (input === 190)){
+          currentVideoResp = true;
+          gameState.videoResps.push(currentVideoResp);
   }
+ });
   //add another key input listener that records answer
 
   //if match(current event, n back), change a dom attribute
-
+function checkForMatch (){
+  var length1 = gameState.audioGens.length;
+  if ((canGuess == true) && (currentAudioResp == true) && (currentAudioResp == gameState.audioGens[length1 - n]{
+    audioScore++;
+    playIfMatched();
+  } else {
+    return false;
+    }
+  var length2 = gameState.videoGens.length;
+  if ((canGuess == true) && (currentVideoResp == true) && (currentVideoResp == gameState.videoGens[length1 - n]{
+    videoScore++;
+    playIfMatched();
+  } else {
+    return false;
+    }
+}
   //do this again for the other match
 
   //set timeout for video display
@@ -113,6 +120,10 @@ function mainEvent(){
 function playIfMatched(){
 var audio = document.getElementById('SP');
 audio.play();
+}
+
+function flashIfMatched(){
+
 }
 
 $(document).keydown(function(event) {
@@ -140,4 +151,24 @@ Record current correctness for both by pushing onto correctness array
 Do this throughout the training
 total correct responses and percentages
 return to home screen
+*/
+
+  /*printState: function() {
+    // console.log(this)
+    if (!gameState.isInProgress) {
+      console.log("Not currently training");
+    } else {
+      console.log(
+        "Lit cell: " + (gameState.litCell === undefined ? "-" : gameState.litCell),
+        "\nCurrent sound: " + (gameState.currentSound === undefined ? "_" : gameState.currentSound),
+        "\nCan guess: " + (game.canGuess === false ? "No" : "Yes"),
+        "\nAudio N-Back: " + ((gameState.canGuess === undefined) || (gameState.videoGens[flashes-n] === undefined)) ? "-" : gameState.audioGens[flashes-n]),
+        "\nVideo N-Back: " + ((gameState.canGuess === undefined) || (gameState.videoGens[flashes-n] === undefined)) ? "-" : gameState.videoGens[flashes-n]),
+        "\nAudio Response: " + (game.currentAudioResp === undefined ? "-" : gameState.currentAudioResp),
+        "\nVideo Response: " + (game.currentVideoResp === undefined ? "-" : gameState.currentVideoResp),
+        "\nAudio Correct: " + ( match(game.currentAudioResp,gameState.audioGens[flashes-n]) === true ? "CORRECT" : "incorrect"),
+        "\nVideo Correct: " + ( match(game.currentAudioResp,gameState.audioGens[flashes-n]) === true ? "CORRECT" : "incorrect"),
+      );
+    }
+  }
 */
