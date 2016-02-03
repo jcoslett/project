7a961cf083a26ad24784b1c276ef0dd10e346dc0
James Coslett <james.coslett@gmail.com>
2016-02-03 12:35:52 -0800

    Forging ahead

--- js/test.js
+++ js/test.js
@@ -1,4 +1,3 @@
-//tests whether js loaded
 console.log("LOADED");
 
 var t;
@@ -8,7 +7,7 @@ var t;
     test: 0,
     n: 2,
     trainingPeriod: 20,
-    speed: 3000,
+    speed: 1000,
     isInProgress: false,
     canGuess: false,
     audioWasGuessed: false,
@@ -27,15 +26,113 @@ var t;
 
 function step() {
   g.counter++;
-  console.log(g.counter);
-  if (g.counter == 20){
-    clearInterval(g.speed);
+  playSound();
+  recordAudioGen();
+  lightCell();
+  recordVideoGen();
+  if (g.counter > g.n){
+    g.canGuess = true;
   }
+  if (g.canGuess == true){
+    setKeyboardListeners();
+  }
+    $(document).keydown(function(event) {
+      var input = event.which; // return which key was pressed
+      if((input === 81) || (input === 87) || (input === 69) ||
+          (input === 82) || (input === 65) || (input === 83) ||
+          (input === 68) || (input === 70) || (input === 90) ||
+          (input === 88) || (input === 67)) {
+              g.audioWasGuessed = true;
+              g.currentAudioResp = true;
+              g.audioResps.push(g.CurrentAudioResp);
+              checkForAudioMatch();
+              if (g.videoWasGuessed == true){ g.canGuess = false;}
+      } else if ((input === 80) || (input === 79) || (input === 73) ||
+          (input === 85) || (input === 74) || (input === 75) ||
+          (input === 76) || (input === 186) || (input === 77) ||
+          (input === 78) || (input === 188) || (input === 190)) {
+              videoWasGuessed = true;
+              currentVideoResp = true;
+              g.videoResps.push(currentVideoResp);
+              checkForVideoMatch();
+              if (audioWasGuessed == true){canGuess = false;}
+        }
+  else {
+    console.log("Can't guess");
+  }
+  console.log(g.currentSound,g.litCell,g.audioGens,g.videoGens,g.canGuess);
+  if (g.counter == 5){
+    clearInterval(t);
+  }
+});
 
+function start() {
+  t = setInterval(step,1000);
 }
 
-function play() {
-  g.speed = setInterval(step,1000);
+//Generates a random sound name from {mstfkh}
+function generateSound(){
+  var sounds = ["m","s","t","f","k","h"];
+  var randomSound = sounds[Math.floor(Math.random()*sounds.length)];
+  return randomSound;
 }
 
-play();
+//plays one of the sounds randomly
+function playSound() {
+  g.currentSound = generateSound();
+  //play the sound
+}
+
+//records the sound played
+function recordAudioGen(){
+  g.audioGens.push(g.currentSound);
+}
+
+//Generate a random cell index from {0:8}
+function generateCell(){
+  var randomCell = Math.floor(Math.random()*9);
+  return randomCell;
+};
+
+//generates a random cell and lights it
+function lightCell() {
+  g.litCell = generateCell();
+  var createdDiv = document.createElement("div");
+  $("#cell" + g.litCell).append(createdDiv);
+}
+
+//records the lit cell
+function recordVideoGen(){
+  g.videoGens.push(g.litCell);
+}
+
+function setKeyboardListeners(){
+  $(document).keydown(function(event) {
+    var input = event.which; // return which key was pressed
+    if((input === 81) || (input === 87) || (input === 69) ||
+       (input === 82) || (input === 65) || (input === 83) ||
+       (input === 68) || (input === 70) || (input === 90) ||
+       (input === 88) || (input === 67)) {
+        g.audioWasGuessed = true;
+        g.currentAudioResp = true;
+        g.audioResps.push(g.CurrentAudioResp);
+        checkForAudioMatch();
+        if (g.videoWasGuessed == true){
+          g.canGuess = false;
+        } else if
+        ((input === 80) || (input === 79) || (input === 73) ||
+        (input === 85) || (input === 74) || (input === 75) ||
+        (input === 76) || (input === 186) || (input === 77) ||
+        (input === 78) || (input === 188) || (input === 190)) {
+          videoWasGuessed = true;
+          currentVideoResp = true;
+          g.videoResps.push(currentVideoResp);
+          checkForVideoMatch();
+          if (audioWasGuessed == true){canGuess = false;}
+          }
+          else {
+            console.log("Can't guess");
+          }
+}
+
+start();
