22359219c6f4df4b329150c1925e4a74d3e33339
James Coslett <james.coslett@gmail.com>
2016-02-04 10:48:12 -0800

    Commit changes

--- js/test.js
+++ js/test.js
@@ -6,12 +6,12 @@ var g = {
     test: 0,
     // n: 2,
     trainingPeriod: 20,
-    speed: 1000,
+    speed: 3350,
     isInProgress: false,
     audioWasGuessed: false,
     videoWasGuessed: false,
-    canGuessAudio: false,
-    canGuessVideo: false,
+    canGuessAudio: false, //can't guess at beginning; initializes to false
+    canGuessVideo: false, //same deal
     litCell: 0,
     currentSound: 0,
     currentAudioResp: null,
@@ -19,33 +19,39 @@ var g = {
     audioScore: 0,
     videoScore: 0,
     audioGens: [],
-    videoGens: []
+    videoGens: [],
+    visMatches: 0,
+    audMatches: 0,
+    audioWronguns: 0,
+    videoWronguns: 0
   }
 
 $('body').keydown(function(event) {
   g.input = event.keyCode; // return which key was pressed
   if(((g.input === 70) || (g.input === 68) || (g.input === 83) ||
      (g.input === 65)) && (g.canGuessAudio === true)) { // left hand audio
-        g.canGuessAudio = false;
         if (g.audioGens[0] === g.audioGens[2]) {
           g.audioScore += 1;
           //play match sound
-        }
-        console.log("Audio match key pressed");
+        } else {
+            g.audioWronguns += 1;
+          }
+
   }
   if (((g.input === 74) || (g.input === 75) || (g.input === 76) ||
      (g.input === 186)) && (g.canGuessVideo === true)) { // right hand visual
-        g.canGuessVideo = false;
       if (g.videoGens[0] === g.videoGens[2]) {
         g.videoScore += 1;
-        //change background
+        //change
+      } else {
+          g.videoWronguns += 1;
       }
-        console.log("Video match key pressed");
-     }
+
+      }
 });
 
 function step() {
-  removeDiv();
+  //removeDiv();
   g.counter++;
   playSound();
   recordAudioGen();
@@ -58,18 +64,29 @@ function step() {
   if (g.counter > 3) {
     g.audioGens.pop();
     g.videoGens.pop();
-    if (g.videoGens[0] === g.videoGens[2]) console.log("Visual Match!");
+    console.log(g.audioScore,g.videoScore,g.audioWronguns,g.videoWronguns);
   }
+  if (g.videoGens[0] === g.videoGens[2]) {
+    g.visMatches += 1;
+    console.log("vis match");
+  }
+  if (g.audioGens[0] === g.audioGens[2]){
+    g.audMatches += 1;
+    console.log("aud match");
+  }
+
   // console.log(g.currentSound,g.litCell,g.audioGens,g.videoGens,g.canGuessAudio,g.canGuessVideo);
   //clearListeners();
   //removeDiv();
+  setTimeout(removeDiv,g.speed/2);
+
   if (g.counter === 25){
     clearInterval(t);
   }
 }
 
 function start() {
-  t = setInterval(step,1000);
+  t = setInterval(step,g.speed);
 }
 
 //Generates a random sound name from {mstfkh}
@@ -87,7 +104,7 @@ function playSound() {
 
 //records the sound played
 function recordAudioGen(){
-  g.audioGens.push(g.currentSound);
+  g.audioGens.unshift(g.currentSound);
 }
 
 //Generate a random cell index from {0:8}
